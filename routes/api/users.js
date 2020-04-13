df6960030427e09726c7eb4ec2318c9ca9148bcc
Brian <brian.holland.pr@gmail.com>
2020-04-13 16:02:59 -0700

    user create and password encryption added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,79 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+    "/",
+    [
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "Please include a valid email").isEmail(),
+        check("password", "Passwords must be 6 or more characters").isLength({
+            min: 6,
+        }),
+    ],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        //check for errors
+        if (!errors.isEmpty()) {
+            //sends back error message from checks
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        //Destructure the body request
+        const { name, email, password } = req.body;
+
+        try {
+            //See if user exists
+            let user = await User.findOne({ email });
+
+            //send error status if user does exist
+            if (user) {
+                res.status(400).json({
+                    errors: [{ msg: "User already exists" }],
+                });
+            }
+
+            //Get users gravatar
+            const avatar = gravatar.url(email, {
+                //size
+                s: "200",
+                //rating
+                r: "pg",
+                //default img
+                d: "mm",
+            });
+
+            //create instance of a new user (unsaved)
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+            });
+
+            //Encrypt password using bcrypt
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            //save user to db
+            await user.save();
+
+            //Return the json webtoken
+            res.send("User route");
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+    }
+);
 
 module.exports = router;
