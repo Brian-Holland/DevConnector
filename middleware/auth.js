9baaa32c15226f2942159dba8c490fdc22d5e5af
Brian <brian.holland.pr@gmail.com>
2020-04-13 18:05:29 -0700

    auth fix and tested

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
-const config = reuire("config");
+const config = require("config");
 
 module.exports = function (req, res, next) {
     //get token from header
     const token = req.header("x-auth-token");
     //check if theres not a token
     if (!token) {
-        return res.status(401).json({ msg: "No token, authorizatio denied" });
+        return res.status(401).json({ msg: "No token, authorization denied" });
     }
     //Verify the token
     try {
