6a0a59989c34fe9dbc57b8dd529d9f972d00ca81
Brian <brian.holland.pr@gmail.com>
2020-04-16 17:40:37 -0700

    move config settings to gitignore

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -26,5 +27,25 @@ router.get("/me", auth, async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post(
+    "/",
+    [
+        auth,
+        [
+            check("status", "Status is required").not().isEmpty(),
+            check("skills", "Skills is required").not().isEmpty(),
+        ],
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+    }
+);
 
 module.exports = router;
